18a3439ec865252b0346c84c0c458171b4606ee9
Vitaliy Ostapenko <soul33d@gmail.com>
2017-05-10 01:45:21 +0300

    Improved removeExtra(String msg), now it removes only non-letter and non-digit symbols duplicates.

--- src/com/homelearning/task5/TerminalHelper.java
+++ src/com/homelearning/task5/TerminalHelper.java
@@ -1,16 +1,20 @@
 package com.homelearning.task5;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class TerminalHelper {
     private Scanner scanner;
+    private Pattern pattern;
 
     public TerminalHelper() {
         scanner = new Scanner(System.in);
+        pattern = Pattern.compile("[\\W]*");
     }
 
     public void execute(){
         System.out.println(removeExtra(askMessage()));
+        scanner.close();
     }
 
     private String removeExtra(String msg){
@@ -18,15 +22,19 @@ public class TerminalHelper {
         StringBuilder stringBuilder = new StringBuilder();
         char[] chars = msg.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if (i == 0) {
-                previous = chars[0];
-                stringBuilder.append(chars[i]);
-            } else previous = chars[i-1];
-            if (previous != chars[i]){
-                stringBuilder.append(chars[i]);
+            if (i == 0) stringBuilder.append(chars[i]);
+            else {
+                previous = stringBuilder.charAt(stringBuilder.length() - 1);
+                if (previous != chars[i] && isSignToRemove(chars[i])) {
+                    stringBuilder.append(chars[i]);
+                } else if (!isSignToRemove(chars[i])) stringBuilder.append(chars[i]);
             }
         }
         return stringBuilder.toString();
+    }
+
+    private boolean isSignToRemove(char c) {
+        return pattern.matcher(String.valueOf(c)).matches();
     }
 
     private String askMessage(){
